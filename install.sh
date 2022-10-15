b58cf9c1d39b50ce5acc7553fd18371a333095ad
Vngosu7 <87461510+Vngosu7@users.noreply.github.com>
2022-10-15 11:02:47 +0700

    Add files via upload

--- install.sh
+++ install.sh
@@ -442,11 +442,14 @@ EOF
 
 # shellcheck disable=SC2183  # printf string has more %s than arguments ($FMT_RAINBOW expands to multiple arguments)
 print_success() {
-  printf '%s         %s__      %s           %s        %s       %s     %s__   %s\n'      $FMT_RAINBOW $FMT_RESET
-  printf '%s  ____  %s/ /_    %s ____ ___  %s__  __  %s ____  %s_____%s/ /_  %s\n'      $FMT_RAINBOW $FMT_RESET
-  printf '%s / __ \\%s/ __ \\  %s / __ `__ \\%s/ / / / %s /_  / %s/ ___/%s __ \\ %s\n'  $FMT_RAINBOW $FMT_RESET
-  printf '%s/ /_/ /%s / / / %s / / / / / /%s /_/ / %s   / /_%s(__  )%s / / / %s\n'      $FMT_RAINBOW $FMT_RESET
-  printf '%s\\____/%s_/ /_/ %s /_/ /_/ /_/%s\\__, / %s   /___/%s____/%s_/ /_/  %s\n'    $FMT_RAINBOW $FMT_RESET
+printf '%s         ╭───────────────────────────────────────────────────────────────────────╮ %s\n'      $FMT_RAINBOW $FMT_RESET
+printf '%s         │     %s██████╗ ███████╗███╗   ██╗██╗██╗  ██╗     ██╗   ██╗███╗   ██╗%s     │%s\n'      $FMT_RAINBOW $FMT_RESET
+printf '%s         │     %s██╔══██╗██╔════╝████╗  ██║██║╚██╗██╔╝     ██║   ██║████╗  ██║%s     │%s\n'      $FMT_RAINBOW $FMT_RESET
+printf '%s         │     %s██████╔╝█████╗  ██╔██╗ ██║██║ ╚███╔╝█████╗██║   ██║██╔██╗ ██║%s     │%s\n'      $FMT_RAINBOW $FMT_RESET
+printf '%s         │     %s██╔══██╗██╔══╝  ██║╚██╗██║██║ ██╔██╗╚════╝╚██╗ ██╔╝██║╚██╗██║%s     │%s\n'      $FMT_RAINBOW $FMT_RESET
+printf '%s         │     %s██║  ██║███████╗██║ ╚████║██║██╔╝ ██╗      ╚████╔╝ ██║ ╚████║%s     │%s\n'      $FMT_RAINBOW $FMT_RESET
+printf '%s         │     %s╚═╝  ╚═╝╚══════╝╚═╝  ╚═══╝╚═╝╚═╝  ╚═╝       ╚═══╝  ╚═╝  ╚═══╝%s     │%s\n'      $FMT_RAINBOW $FMT_RESET
+printf '%s         ╰───────────────────────────────────────────────────────────────────────╯%s\n'      $FMT_RAINBOW $FMT_RESET
   printf '%s    %s        %s           %s /____/ %s       %s     %s          %s....is now installed!%s\n' $FMT_RAINBOW $FMT_GREEN $FMT_RESET
   printf '\n'
   printf '\n'
